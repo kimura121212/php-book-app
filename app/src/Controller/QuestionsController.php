f4432b536b33d539e9fa9710531537eea507d77e
kimura121212 <ykimuranoi@gmail.com>
2019-10-11 03:52:16 +0900

    質問投稿画面実装

--- app/src/Controller/QuestionsController.php
+++ app/src/Controller/QuestionsController.php
@@ -21,4 +21,26 @@ class QuestionsController extends AppController
 
         $this->set(compact('questions'));
     }
+
+    // 質問投稿画面/質問投稿処理
+    // @return \Cake\Http\Response|null 質問投稿後に質問一覧画面へ遷移する
+
+    public function add()
+    {
+        $question = $this->Questions->newEntiry();
+
+        if ($this->request->is('post')){
+            $question = $this->Questions->patchEntity($question, $this->request->getData());
+            $question->user_id = 1; // @TODOユーザー管理機能実装時に修正する
+
+            if ($this->Questions->save($question)){
+                $this->Flash->success('質問を投稿しました');
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error('質問の投稿に失敗しました');
+        }
+        $this->set(compact('question'));
+
+    }
 }
